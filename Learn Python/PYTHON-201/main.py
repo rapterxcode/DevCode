2a07e803768ab9c3b4df6ae6d4adc0cc3c5d8609
rapterxcode <phonepasit9@gmail.com>
2025-06-21 06:32:53 +0700

    main

---
+++ Learn Python/PYTHON-201/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+nums1 = np.array([1, 2, 3, 4, 5])
+nums2 = np.array([6, 7, 8, 9, 10])
+
+print("Plus",nums1 + nums2)
+print("Minus",nums1 - nums2)
+print("Multiply",nums1 * nums2)
+print("Divide",nums1 / nums2)
+print("Power",nums1 ** nums2)
+print("Modulus",nums1 % nums2)
+print("Floor Division",nums1 // nums2)
+print("Power",nums1 ** nums2)
+
+print("Mean",nums1.mean())
+print("Standard Deviation",nums1.std())
+print("Variance",nums1.var())
+print("Minimum",nums1.min())
+print("Maximum",nums1.max())
+print("Sum",nums1.sum())
+
+
+
+
+
