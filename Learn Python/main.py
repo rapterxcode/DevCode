0f96e4a8b929211d72274fe3f6d87ac838b3f3c8
rapterxcode <phonepasit9@gmail.com>
2025-06-21 05:07:33 +0700

    first commit

---
+++ Learn Python/main.py
@@ -0,0 +1,13 @@
+height = 170.5
+weight = 70
+name = "John"
+print("My name is", name, "and my height is", height, "and my weight is", weight)
+
+
+# 1. Create a variable called "name" and assign it the value "John"
+# 2. Create a variable called "height" and assign it the value 170.5
+# 3. Create a variable called "weight" and assign it the value 70
+# 4. Print the value of the variable "name"
+# 5. Print the value of the variable "height"
+# 6. Print the value of the variable "weight"
+
